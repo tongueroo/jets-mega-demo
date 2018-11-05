641531a8d9b7f6ab46b731ee16869c3a14824def
Tung Nguyen <tongueroo@gmail.com>
2018-11-05 01:42:25 +0000

    add files generated from jets import:rails --submodule

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Jets.application.routes.draw do
   # Note, as part of the deploy process Jets uploads files in the public folder to s3
   # and serves them out of s3 directly. S3 is well suited to serve static assets.
   # More info here: http://rubyonjets.com/docs/assets-serving/
-  any "*catchall", to: "jets/public#show"
+  # any "*catchall", to: "jets/public#show"
+  # Enables Mega Mode Rails integration
+  any "*catchall", to: "jets/rack#process"
 end
