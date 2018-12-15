29173582f3af26e96afd47c7ba8ab4f4a3c84ba6
Tung Nguyen <tongueroo@gmail.com>
2018-12-15 03:34:26 +0000

    upgrade jets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,2 @@
 Jets.application.configure do
-  config.ruby.lazy_load = true
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,3 @@
 Jets.application.configure do
   # If your app exceeds the AWS Lambda code size limit then Jets will automatically
-  # enable config.ruby.lazy_load = true regardless of this setting.
-  config.ruby.lazy_load = false
 end
